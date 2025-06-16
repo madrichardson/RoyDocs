7f18f93b5e591d31963a460e4f336dca8dcca492
madrichardson <madison.richardson@noaa.gov>
2025-06-16 10:18:41 -0700

    Add royscripts to Sphinx and edit .rst

--- sphinx-src/source/conf.py
+++ sphinx-src/source/conf.py
@@ -3,10 +3,18 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+
+# Insert the parent dir so that "roy-scripts" is on sys.path
+sys.path.insert(0, os.path.abspath('/Users/madisonrichardson/Roy/roy-scripts'))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Documentation of Roy's Scripts"
+project = "Roy Scripts API"
 copyright = '2025, Roy Mendelssohn & Madison Richardson'
 author = 'Roy Mendelssohn & Madison Richardson'
 release = '0.1'
@@ -14,7 +22,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = []
